1c0cd0bd6457279bfd02a71649d69ef0f2699f61
rit894 <nagarithwikv@gmail.com>
2026-03-03 19:55:30 +0530

    Remove commented-out SQL statements and retain distinct selection for student records

--- MYSQL LOCAL DB1.session.sql
+++ MYSQL LOCAL DB1.session.sql
@@ -1,24 +1,2 @@
--- CREATE TABLE student(
---     student_id INT PRIMARY KEY,
---     name VARCHAR(20),
---     major VARCHAR(20)
--- );
-
--- this is used to describe the table and the feilds of the table
--- DESCRIBE student;
-
--- this is used to delete the  particular table from the database
--- DROP TABLE student;
-
--- this is used to add a new column to the existing table
--- ALTER TABLE student ADD gpa DECIMAL(3,2);
--- DESCRIBE student;
-
-SELECT * FROM student;
-
- -- tHIS IS USED TO
--- INSERT INTO student VALUES(2, 'kate', 'Chemical Engg', 5.0);
-
-
-
+SELECT DISTINCT * FROM student;
 
